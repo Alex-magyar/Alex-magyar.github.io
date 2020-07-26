566f09476f39640dd99ae822ca94eeaf073b8989
Alex <csaalex@freemail.hu>
2020-07-26 09:32:14 +0200

    2

---
+++ javascript/js.js
@@ -0,0 +1,27 @@
+let inpName = document.querySelector("input[name='name']");
+inpName.value;
+console.log(inpName.value);
+
+function jokeAllert() {
+       document.querySelector("button[name='gombo']");
+     alert("Maistro, music!");
+
+
+    
+} }
+
+
+function szamolas() {
+    let pricePerCapita = 5000;
+    let amountImput = document.querySelector("form#diabaz input[name='kacsaparadicsom']");
+    let eredmeny = document.querySelector("form#diabaz .message strong");
+    let amount = parseInt(amountInput.value);
+    let totalAmount = amount * pricePerCapita;
+    let showAmount.innerHTML = totalAmount;
+
+eredmeny.innerHTML = HUF( totalAmount);
+
+}
+    
+
+
